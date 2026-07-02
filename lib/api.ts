72a7c85a39f00cca0d9447af886ada5ba74860ee
vidhi1004 <vidhishrimal04@gmail.com>
2026-07-02 22:16:40 +0530

    frontend-url-change

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
-const rawUrl =
+const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL ||
   "https://localmart-api-gateway.onrender.com";
 
-export const API_BASE_URL = rawUrl.endsWith("/") ? rawUrl : `${rawUrl}/`;
+// export const API_BASE_URL = rawUrl.endsWith("/") ? rawUrl : `${rawUrl}/`;
