04d552eb304f216dd8606f32ce905dc144e5ae7e
vidhi1004 <vidhishrimal04@gmail.com>
2026-07-02 19:08:36 +0530

    frontend-changes

--- lib/api.ts
+++ lib/api.ts
@@ -1,2 +1,3 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  "https://localmart-api-gateway.onrender.com";
