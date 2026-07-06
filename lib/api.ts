fdcd3740cd35987506b36b0fb605f206d7de444b
vidhi1004 <vidhishrimal04@gmail.com>
2026-07-06 12:00:37 +0530

    link

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,4 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ||
-  "http://localhost:3000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
 
 // export const API_BASE_URL = rawUrl.endsWith("/") ? rawUrl : `${rawUrl}/`;
